c2cc9211bd5f8409fa1457c16ad1cf9d9956d7ae
Yousef <yousefayman2000500@gmail.com>
2024-01-13 01:58:31 +0200

    using ref to make more DOM connections

--- src/components/TimeChallenge.jsx
+++ src/components/TimeChallenge.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const TimeChallenge = ({ title, targetTime }) => {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
+  const timer = useRef();
+
   const handleStart = () => {
     setTimerStarted(true);
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
+  };
+  const handleStop = () => {
+    clearTimeout(timer.current);
+    setTimerStarted(false);
   };
   return (
     <section className="challenge">
@@ -18,7 +24,7 @@ const TimeChallenge = ({ title, targetTime }) => {
         {timerExpired && <p>You lost!</p>}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "stop" : "start"} challenge
         </button>
       </p>
